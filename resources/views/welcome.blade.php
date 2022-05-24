58e4bdca24949a2382b634f658c771e8fabbd527
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2022-05-24 14:00:10 +0400

    change welcome page v6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -510,7 +510,7 @@
                         </svg>
 
                         <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                            DigitalSail - v5.0
+                            DigitalSail - v6.0
                         </a>
                     </div>
                 </div>
